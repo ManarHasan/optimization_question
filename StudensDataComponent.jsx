a9aaca4f1c0ffa43fe8dbfc156fc236d883a4da5
manarHasan <manar.hasan62@gmail.com>
2023-05-13 19:33:16 +0300

    add comments

--- StudensDataComponent.jsx
+++ StudensDataComponent.jsx
@@ -10,11 +10,16 @@ const studentsDataComponent = () => {
   const [legalguardiansData, setLegalguardiansData] = useState([]);
 
 
+  // promise.all is faster than awaiting seperate api calls.
   const onStudentsPick = async (studentIds) => {
-    for (const studentId of studentIds) {
-      const studentData = await fetchStudentData(studentId);
-      setStudentsData(() => [...studentsData, {...studentData, processed: false}]);
-    }
+    await Promise.all(studentIds.map(async (studentId) => {
+        const studentData = await fetchStudentData(studentId);
+        // when updating with a function it should be instantaneous and not be updated on the next iteration
+        // add the processed field to not re-fetch the other students data in the next map
+        setStudentsData(() => [...studentsData, {...studentData, processed: false}]);
+    }))
+
+    // make sure to set processed to true for the next time this function is called
     const processedStudentData = studentsData.map(async (student) => {
         const { schoolId, legalguardianId } = student;
         const [schoolData, legalguardianData] = await Promise.all([
